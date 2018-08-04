8b50103b63a1b1bbe06a42d2a249296f9889e234
LucasArbre <lucas.sg14@hotmail.com>
2018-08-04 00:22:44 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/arbresystems/appoint/Teste.java

--- app/src/main/java/com/arbresystems/appoint/Teste.java
+++ app/src/main/java/com/arbresystems/appoint/Teste.java
@@ -2,4 +2,7 @@ package com.arbresystems.appoint;
 
 public class Teste {
     private String batata;
+    private String mano ="quero dormir véi KKKKKK";
+    private String PUTA_SONO_PIA;
+    private int macarrao;
 }
