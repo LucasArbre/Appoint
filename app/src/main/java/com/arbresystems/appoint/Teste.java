afa2f15dff5d6dd10be919873f1150b279fca067
Rubinho <rubinho142@outlook.com.br>
2018-08-04 00:39:08 -0300

    uau

--- app/src/main/java/com/arbresystems/appoint/Teste.java
+++
@@ -1,8 +0,0 @@
-package com.arbresystems.appoint;
-
-public class Teste {
-    private String batata;
-    private String mano ="quero dormir véi KKKKKK";
-    private String PUTA_SONO_PIA="vamo dormi logo mano";
-    private int macarrao;
-}
