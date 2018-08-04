ee7a1dd7482feaee4045cd26cf7494956d5708d1
LucasArbre <lucas.sg14@hotmail.com>
2018-08-04 00:26:05 -0300

    katiau

--- app/src/main/java/com/arbresystems/appoint/Teste.java
+++ app/src/main/java/com/arbresystems/appoint/Teste.java
@@ -3,6 +3,6 @@ package com.arbresystems.appoint;
 public class Teste {
     private String batata;
     private String mano ="quero dormir véi KKKKKK";
-    private String PUTA_SONO_PIA;
+    private String PUTA_SONO_PIA="vamo dormi logo mano";
     private int macarrao;
 }
