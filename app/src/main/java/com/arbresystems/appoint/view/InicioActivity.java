3ff4e986536856054e0938895ec5b9d97f512cd3
Rubinho <rubinho142@outlook.com.br>
2018-08-14 22:40:00 -0300

    tão me chamando pro x1 no truco, amanhã eu termino

--- app/src/main/java/com/arbresystems/appoint/view/InicioActivity.java
+++ app/src/main/java/com/arbresystems/appoint/view/InicioActivity.java
@@ -1,16 +1,38 @@
 package com.arbresystems.appoint.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.arbresystems.appoint.R;
 
 public class InicioActivity extends AppCompatActivity {
+    private Button btnCadastrar;
+    private Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_inicio);
+
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CadastrarActivity.class));
+            }
+        });
     }
 }
