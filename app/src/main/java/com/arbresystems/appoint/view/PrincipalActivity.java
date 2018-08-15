7a66b772655aa377d83d6407c96ec7f47340404e
LucasArbre <lucas.sg14@hotmail.com>
2018-08-14 23:19:44 -0300

    imagem

--- app/src/main/java/com/arbresystems/appoint/view/PrincipalActivity.java
+++ app/src/main/java/com/arbresystems/appoint/view/PrincipalActivity.java
@@ -1,15 +1,65 @@
 package com.arbresystems.appoint.view;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.arbresystems.appoint.R;
 
 public class PrincipalActivity extends AppCompatActivity {
 
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+    private HomeFragment homeFragment;
+    private SearchFragment searchFragment;
+    private MenuFragment menuFragment;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        mMainFrame =(FrameLayout)findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        searchFragment = new SearchFragment();
+        menuFragment = new MenuFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.nav_home:
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.nav_menu:
+                        setFragment(menuFragment);
+                        return true;
+                    case R.id.nav_search:
+                        setFragment(searchFragment);
+                        return true;
+
+                        default: return false;
+                }
+
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
