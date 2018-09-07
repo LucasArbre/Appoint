8d318ea2e5e0d3c804f2c28bbde117673587c2b7
LucasArbre <lucas.sg14@hotmail.com>
2018-09-06 23:28:34 -0300

    *Layout da tela de Loading adicionada *Listeners adicionados a configurações *botoes de teste adicionados ao fragmento menu para testar as configurações -Não consegui realizar o teste devido ao login não estar funcionando.

--- app/src/main/java/com/arbresystems/appoint/view/ConfigActivity.java
+++ app/src/main/java/com/arbresystems/appoint/view/ConfigActivity.java
@@ -1,8 +1,12 @@
 package com.arbresystems.appoint.view;
 
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 import com.arbresystems.appoint.R;
 
@@ -20,6 +24,51 @@ public class ConfigActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            bindSummaryValue(findPreference("key_nome_usr"));
+            bindSummaryValue(findPreference("key_email_usr"));
+            bindSummaryValue(findPreference("key_telefone_usr"));
+            bindSummaryValue(findPreference("key_tempo_config"));
+
+            btnDesloga(findPreference("key_sair_conta"));
         }
     }
+
+    private static void bindSummaryValue(Preference preference){
+        preference.setOnPreferenceChangeListener(listener);
+        listener.onPreferenceChange(preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    private static Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValor = newValue.toString();
+            if (preference instanceof ListPreference){
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValor);
+                //seta o sumario com o novo valor
+                preference.setSummary(index > 0
+                    ? listPreference.getEntries()[index]
+                    :null);
+
+            }else if (preference instanceof EditTextPreference){
+                preference.setSummary(stringValor);
+            }
+            return true;
+        }
+    };
+
+    private static void btnDesloga(Preference preference){
+        preference.setOnPreferenceClickListener(actListener);
+    }
+
+    private static Preference.OnPreferenceClickListener actListener = new Preference.OnPreferenceClickListener() {
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            //codigo de deslogar
+            return false;
+        }
+    };
 }
