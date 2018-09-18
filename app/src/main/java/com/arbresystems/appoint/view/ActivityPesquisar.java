6de2a415d727f256d260309e8e878602b597eedd
LucasArbre <lucas.sg14@hotmail.com>
2018-09-18 06:42:36 -0300

    erro n sei onde

--- app/src/main/java/com/arbresystems/appoint/view/ActivityPesquisar.java
+++ app/src/main/java/com/arbresystems/appoint/view/ActivityPesquisar.java
@@ -6,18 +6,34 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.arbresystems.appoint.BottomNavigationViewHelper;
 import com.arbresystems.appoint.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ActivityPesquisar extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesquisar);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+
+        ListView lv = findViewById(R.id.pesquisaAdm);
+        ArrayList<String> arrayEstabelecimentos = new ArrayList<>();
+        arrayEstabelecimentos.addAll(Arrays.asList(getResources().getStringArray(R.array.my_estabelecimentos)));
+        adapter = new ArrayAdapter<>(ActivityPesquisar.this, android.R.layout.simple_list_item_1, arrayEstabelecimentos);
+        lv.setAdapter(adapter);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView_Bar);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(0);
@@ -45,5 +61,27 @@ public class ActivityPesquisar extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.menuPesquisa);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
