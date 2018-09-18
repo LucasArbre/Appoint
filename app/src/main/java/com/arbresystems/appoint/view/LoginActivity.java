fbbff26dea97a09a879e4fa7400db0e1d2c0c0b4
Rubinho <rubinho142@outlook.com.br>
2018-09-18 12:06:53 -0300

    otimização de imports

--- app/src/main/java/com/arbresystems/appoint/view/LoginActivity.java
+++ app/src/main/java/com/arbresystems/appoint/view/LoginActivity.java
@@ -3,20 +3,15 @@ package com.arbresystems.appoint.view;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.arbresystems.appoint.R;
@@ -50,8 +45,6 @@ import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.TimeUnit;
 
 import retrofit2.Call;
