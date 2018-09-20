d9bede690921667db32309ed05da7b6ee5db4637
Rubinho <rubinho142@outlook.com.br>
2018-09-20 14:35:58 -0300

    logout

--- app/src/main/java/com/arbresystems/appoint/view/PrincipalActivity.java
+++ app/src/main/java/com/arbresystems/appoint/view/PrincipalActivity.java
@@ -1,24 +1,29 @@
 package com.arbresystems.appoint.view;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.arbresystems.appoint.BottomNavigationViewHelper;
 import com.arbresystems.appoint.R;
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static com.arbresystems.appoint.view.MainActivity.PREF_NAME;
 
 public class PrincipalActivity extends AppCompatActivity {
 
     private BottomNavigationView mMainNav;
     private FrameLayout mMainFrame;
+
+    private FirebaseAuth mAuth;
+    private SharedPreferences sp;
 
 
     @Override
@@ -30,6 +35,9 @@ public class PrincipalActivity extends AppCompatActivity {
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
+
+        mAuth = FirebaseAuth.getInstance();
+        sp = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -75,8 +83,12 @@ public class PrincipalActivity extends AppCompatActivity {
             startActivity(intent);
         }
         if(id==R.id.opt_sair){
-            //codigo de logout
-            Toast.makeText(this,  "Você esta preso aqui", Toast.LENGTH_SHORT).show();
+            mAuth.signOut();
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("token", null);
+            editor.apply();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
         }
         return super.onOptionsItemSelected(item);
     }
