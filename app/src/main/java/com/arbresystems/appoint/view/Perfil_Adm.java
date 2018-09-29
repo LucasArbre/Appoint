3bf9624b8f9228b896d39609b20ea4152ac83415
LucasArbre <lucas.sg14@hotmail.com>
2018-09-29 02:18:36 -0300

    Tela de perfil adm adicionada.Falta por funcionalidade nos botoes e o adapter na lv. Vou dormir& minha ide bugou

---
+++ app/src/main/java/com/arbresystems/appoint/view/Perfil_Adm.java
@@ -0,0 +1,16 @@
+package com.arbresystems.appoint.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arbresystems.appoint.R;
+
+public class Perfil_Adm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil__adm);
+        getSupportActionBar().hide();
+    }
+}
---
+++ app/src/main/java/com/arbresystems/appoint/view/Promocao_tela_individual.java
@@ -0,0 +1,22 @@
+package com.arbresystems.appoint.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.arbresystems.appoint.R;
+
+public class Promocao_tela_individual extends AppCompatActivity {
+
+    private TextView txtTitulo;
+    private TextView txtDescricao;
+    private String imagem;
+    private int valor_promocao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promocao_tela_individual);
+        getSupportActionBar().hide();
+    }
+}
