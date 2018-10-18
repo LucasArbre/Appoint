5bf0a393389940f3dc018c72b7e55f87878be4f4
Rubinho <rubinho142@outlook.com.br>
2018-10-18 00:53:19 -0300

    - cadastro com google - concluido - cadastro com telefone - concluido - cadastro com facebook - falta pegar o email do usuario - login com google - concluido - login com telefone - concluido - login com facebook - falta pegar o email do usuario - alterações nas configurações -  concluído - inicio do serviço de atualização de localização - falta pedir acesso as permisões - perfil adm -        - no banco de dados             - incluir imagem de perfil             - horario de funcionamento
    - pesquisa - concluido
    - informações perfil adm -
           - no banco de dados
                - data que entrou no app
            - na api
                - retornar a media de avaliação

--- app/src/main/java/com/arbresystems/appoint/view/InformacoesAdmActivity.java
+++ app/src/main/java/com/arbresystems/appoint/view/InformacoesAdmActivity.java
@@ -1,15 +1,32 @@
 package com.arbresystems.appoint.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.arbresystems.appoint.R;
+import com.arbresystems.appoint.model.Estabelecimento;
 
 public class InformacoesAdmActivity extends AppCompatActivity {
+
+    private Estabelecimento estabelecimento;
+
+    private TextView txtNome;
+    private TextView txtDescricao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacoes_adm);
+
+        Intent intent = getIntent();
+        estabelecimento = (Estabelecimento) intent.getSerializableExtra("estabelecimento");
+
+        txtDescricao = findViewById(R.id.txtDescricao);
+        txtNome = findViewById(R.id.txtNome);
+
+        txtDescricao.setText(estabelecimento.getDescricao());
+        txtNome.setText(estabelecimento.getNome());
     }
 }
