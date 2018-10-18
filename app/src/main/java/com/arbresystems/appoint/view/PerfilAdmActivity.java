8de0f074b05da47b4d7f425f534c9f72eb20d89d
Rubinho <rubinho142@outlook.com.br>
2018-10-18 00:41:13 -0300

    - cadastro com google - concluido - cadastro com telefone - concluido - cadastro com facebook - falta pegar o email do usuario - login com google - concluido - login com telefone - concluido - login com facebook - falta pegar o email do usuario - alterações nas configurações -  concluído - inicio do serviço de atualização de localização - falta pedir acesso as permisões - perfil adm -        - no banco de dados             - incluir imagem de perfil             - horario de funcionamento             - data que entrou no app        - na api             - retornar a media de avaliação
    - pesquisa - concluido
    - informações perfil adm -

--- app/src/main/java/com/arbresystems/appoint/view/PerfilAdmActivity.java
+++ app/src/main/java/com/arbresystems/appoint/view/PerfilAdmActivity.java
@@ -13,6 +13,11 @@ import com.arbresystems.appoint.R;
 import com.arbresystems.appoint.model.Estabelecimento;
 import com.bumptech.glide.Glide;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class PerfilAdmActivity extends AppCompatActivity {
 
     private Estabelecimento estabelecimento;
@@ -22,6 +27,8 @@ public class PerfilAdmActivity extends AppCompatActivity {
     private Button btnAgendarHorario;
     private Button btnIrPara;
     private Button btnSobre;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,8 @@ public class PerfilAdmActivity extends AppCompatActivity {
         btnSobre = findViewById(R.id.btnSobre);
 
         Glide.with(this).load("http://goo.gl/gEgYUd").into(imgPerfil);
+
+        Log.e("dia", getDia());
 
         btnLigar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,5 +73,34 @@ public class PerfilAdmActivity extends AppCompatActivity {
                 startActivity( new Intent(getApplicationContext(), InformacoesAdmActivity.class).putExtra("estabelecimento", estabelecimento));
             }
         });
+    }
+
+    private String getDia(){
+        Date d = new Date();
+        Calendar c = new GregorianCalendar();
+        c.setTime(d);
+        String nome = "";
+        int dia = c.get(c.DAY_OF_WEEK);
+        switch(dia){
+            case Calendar.SUNDAY: nome = "Domingo";break;
+            case Calendar.MONDAY: nome = "Segunda";break;
+            case Calendar.TUESDAY: nome = "Terça";break;
+            case Calendar.WEDNESDAY: nome = "Quarta";break;
+            case Calendar.THURSDAY: nome = "Quinta";break;
+            case Calendar.FRIDAY: nome = "Sexta";break;
+            case Calendar.SATURDAY: nome = "sábado";break;
+        }
+        return nome;
+    }
+
+    private String getHorario(){
+        SimpleDateFormat dateFormatHora = new SimpleDateFormat("HH:mm:ss");
+        Date data = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(data);
+        Date dataAtual = cal.getTime();
+        String horaAtual = dateFormatHora.format(dataAtual);
+        Log.i("hora", horaAtual);
+        return horaAtual;
     }
 }
