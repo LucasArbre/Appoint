7712a078407ae1175cb316f9189aea1c58e5edc3
LucasArbre <lucas.sg14@hotmail.com>
2018-10-23 00:04:09 -0300

    update basico: Nova tela serviços; Novo botão "agendar um horario" na activity principal; Pesquisa por serviços na tela serviços; Edição na activity compromisso, que agora possui atributos para ser diretamente editada.

---
+++ app/src/main/java/com/arbresystems/appoint/view/ServicosActivity.java
@@ -0,0 +1,65 @@
+package com.arbresystems.appoint.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.arbresystems.appoint.R;
+import com.arbresystems.appoint.model.Servicos;
+import com.arbresystems.appoint.viewModels.RecyclerViewDataAdapter;
+import com.arbresystems.appoint.viewModels.RecyclerViewDataAdapterHorario;
+import com.arbresystems.appoint.viewModels.RecyclerViewDataAdapterServicos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicosActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Servicos> servicos;
+    private RecyclerViewDataAdapterServicos adapter = new RecyclerViewDataAdapterServicos(this, servicos);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servicos);
+
+        recyclerView = findViewById(R.id.rvServicos);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //adapter = new RecyclerViewDataAdapterServicos(this, servicos);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.search_menu,menu);
+        MenuItem menuItem = menu.findItem(R.id.menuPesquisa);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        String userInput = s.toLowerCase();
+        ArrayList<Servicos> newList = new ArrayList<>();
+
+        for (Servicos servico : servicos) {
+            if (servico.getNome().toLowerCase().contains(userInput)) {
+                newList.add(servico);
+            }
+        }
+        adapter.updateList(newList);
+        return false;
+    }
+}
