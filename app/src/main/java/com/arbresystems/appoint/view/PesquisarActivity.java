abf99cbd38d0e135e349510fe7724e4f64b073f5
LucasArbre <lucas.sg14@hotmail.com>
2018-10-28 03:10:27 -0300

    bagui de loading em pesquisar

--- app/src/main/java/com/arbresystems/appoint/view/PesquisarActivity.java
+++ app/src/main/java/com/arbresystems/appoint/view/PesquisarActivity.java
@@ -3,6 +3,7 @@ package com.arbresystems.appoint.view;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +13,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.SearchView;
 
 import com.arbresystems.appoint.R;
@@ -32,9 +36,13 @@ import static com.arbresystems.appoint.view.MainActivity.PREF_NAME;
 public class PesquisarActivity extends AppCompatActivity {
 
     private SharedPreferences sp;
-    private ListView listView;
     private AdapterItemPesquisaEstabelecimento adapter;
     private RecyclerView recyclerView;
+    private LinearLayoutManager manager;
+    private boolean isScrolling = false;
+    private List<Estabelecimento> estabelecimentos;
+    private int currentItem, totalIems, scrollOutItems;
+    private ProgressBar progressBar = findViewById(R.id.progress);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +52,9 @@ public class PesquisarActivity extends AppCompatActivity {
         sp = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
 
         //listView = findViewById(R.id.pesquisaAdm);
-
+        manager = new LinearLayoutManager(this);
         recyclerView = findViewById(R.id.rvPesquisa);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(manager);
         //adapter = new RecyclerViewDataAdapterServicos(this, servicos);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView_Bar);
@@ -75,7 +83,46 @@ public class PesquisarActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                    isScrolling = true;
+                }
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                currentItem = manager.getChildCount();
+                totalIems = manager.getItemCount();
+                scrollOutItems = manager.findFirstVisibleItemPosition();
+
+                if (isScrolling && (currentItem+scrollOutItems == totalIems)){
+                    isScrolling = false;
+                    fetchData();
+                }
+            }
+
+        });
     }
+
+    private void fetchData() {
+        progressBar.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0;i<5;i++){
+                    //estabelecimentos.add();
+                    adapter.notifyDataSetChanged();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        },5000);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -92,7 +139,7 @@ public class PesquisarActivity extends AppCompatActivity {
                 new RetrofitConfig().getEstabelecimentoService().pesquisar(sp.getString("token", null), s).enqueue(new Callback<ArrayList<Estabelecimento>>() {
                     @Override
                     public void onResponse(Call<ArrayList<Estabelecimento>> call, Response<ArrayList<Estabelecimento>> response) {
-                        List<Estabelecimento> estabelecimentos = response.body();
+                        estabelecimentos = response.body();
                         Log.e("resposta", estabelecimentos.toString());
                         adapter = new AdapterItemPesquisaEstabelecimento(getApplicationContext(), estabelecimentos);
                         //listView.setAdapter(adapter);
