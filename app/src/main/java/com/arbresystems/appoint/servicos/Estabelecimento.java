53df7f9004f6454ec2c5dffb29a1eca8bc4bb4eb
Rubinho <rubinho142@outlook.com.br>
2018-11-06 21:31:09 -0200

    - cadastro com google - concluido - cadastro com telefone - concluido - cadastro com facebook - falta pegar o email do usuario - login com google - concluido - login com telefone - concluido - login com facebook - falta pegar o email do usuario - alterações nas configurações -  concluído - inicio do serviço de atualização de localização - testando - perfil adm -        - no banco de dados             - horario de funcionamento - pesquisa - falta alterar a img perfil, fazer o calculo de distancia e implementar o load - informações perfil adm - concluido - tela principal promoções - concluido (tera alterações depois) - configurar Firebase Perfomance - concluido - corrigir erro de inicialização - - tela com serviços - - tela principal - - tela agendar - - cadastro adm - - login adm -

--- app/src/main/java/com/arbresystems/appoint/servicos/Estabelecimento.java
+++ app/src/main/java/com/arbresystems/appoint/servicos/Estabelecimento.java
@@ -13,6 +13,14 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface Estabelecimento {
+    //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6ImFwaUFwcG9pbnQifQ.eyJpc3MiOiJodHRwOlwvXC9hcmJyZXN5c3RlbXMuY29tIiwiYXVkIjoiaHR0cDpcL1wvYXBpLmFwcG9pbnQuYXJicmVzeXN0ZW1zLmNvbSIsImp0aSI6ImFwaUFwcG9pbnQiLCJpYXQiOjE1MzEwODcyNDksIm5iZiI6MTUzMTA4NzI1OSwiY2FkYXN0cm9Fc3RhYmVsZWNpbWVudG8iOiJvayJ9.9AJOO7q1f1T7SKX6V9iV10r0rbaZz-SB8Nn8J-u4jBs
+    @Headers({
+            "x-authentication: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6ImFwaUFwcG9pbnQifQ.eyJpc3MiOiJodHRwOlwvXC9hcmJyZXN5c3RlbXMuY29tIiwiYXVkIjoiaHR0cDpcL1wvYXBpLmFwcG9pbnQuYXJicmVzeXN0ZW1zLmNvbSIsImp0aSI6ImFwaUFwcG9pbnQiLCJpYXQiOjE1MzEwODcyNDksIm5iZiI6MTUzMTA4NzI1OSwiY2FkYXN0cm9Fc3RhYmVsZWNpbWVudG8iOiJvayJ9.9AJOO7q1f1T7SKX6V9iV10r0rbaZz-SB8Nn8J-u4jBs",
+            "x-audience: http://api.appoint.arbresystems.com",
+            "x-id: apiAppoint"
+    })
+    @POST("/inserirEstabelecimento")
+    Call<com.arbresystems.appoint.model.Estabelecimento> cadastro(@Body com.arbresystems.appoint.model.Estabelecimento estabelecimento);
 
     @Headers({
             "x-audience: http://api.appoint.arbresystems.com",
