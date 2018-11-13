11e9f5bc83e34115e5a113d5376511e218e26756
Rubinho <rubinho142@outlook.com.br>
2018-11-13 01:48:49 -0200

    - cadastro com google - concluido - cadastro com telefone - concluido - cadastro com facebook - falta pegar o email do usuario - login com google - concluido - login com telefone - concluido - login com facebook - falta pegar o email do usuario - alterações nas configurações -  concluído - inicio do serviço de atualização de localização - testando - perfil adm -        - no banco de dados             - horario de funcionamento - pesquisa - falta alterar a img perfil, fazer o calculo de distancia e implementar o load - informações perfil adm - concluido - tela principal promoções - concluido (tera alterações depois) - configurar Firebase Perfomance - concluido - corrigir erro de inicialização - - tela com serviços -        - tela com os estabelecimentos - - tela principal - - tela agendar - - resolver erros de atualziação de usuário cliente - cadastro adm - - login adm -

--- app/src/main/java/com/arbresystems/appoint/viewAdm/PrincipalAdmActivity.java
+++ app/src/main/java/com/arbresystems/appoint/viewAdm/PrincipalAdmActivity.java
@@ -3,6 +3,8 @@ package com.arbresystems.appoint.viewAdm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.arbresystems.appoint.R;
+
 public class PrincipalAdmActivity extends AppCompatActivity {
 
     @Override
